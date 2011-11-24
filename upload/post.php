24d9f603ee7bad9a4f69b1d76259c45f6bdd4e27
Nathan Findley <nathan@zenlok.com>
2011-11-24 18:43:31 +0900

    Cleanup.

--- upload/post.php
+++ upload/post.php
@@ -4,11 +4,7 @@ $home  = preg_replace("/demo.4ydx.com/", '', $home);
 $index = preg_replace('/[^A-Za-z0-9_]/', '', $_SERVER['HTTP_X_ID']);
 file_put_contents($home.'uploads/'.$index, file_get_contents("php://input"));
 header("X-ID: {$_SERVER['HTTP_X_ID']}");
-header("X-INDEX: {$_SERVER['HTTP_X_DISPLAY_INDEX']}");
 header("X-FILE-SIZE: {$_SERVER['HTTP_X_FILE_SIZE']}");
 header("X-FILE-NAME: {$_SERVER['HTTP_X_FILE_NAME']}");
-echo '{ "status" : "OK", '.
-  '"id"    : "'.$_SERVER['HTTP_X_ID'].'", '.
-  '"index" : "'.$_SERVER['HTTP_X_INDEX'].
-  '" }';
+echo '{ "status" : "OK", "id" : "'.$_SERVER['HTTP_X_ID'].'" }';
 ?>
