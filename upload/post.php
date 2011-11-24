734c11b32a5d30a6a2fef67e1e492aab1917148b
Nathan Findley <nathan@zenlok.com>
2011-11-24 18:38:58 +0900

    Do a bit of sanitation.

--- upload/post.php
+++ upload/post.php
@@ -1,7 +1,7 @@
 <?php
 $home  = $_SERVER['DOCUMENT_ROOT']; 
 $home  = preg_replace("/demo.4ydx.com/", '', $home);
-$index = $_SERVER['HTTP_X_ID'];
+$index = preg_replace('/[^A-Za-z0-9_]/', '', $_SERVER['HTTP_X_ID']);
 file_put_contents($home.'uploads/'.$index, file_get_contents("php://input"));
 header("X-ID: {$_SERVER['HTTP_X_ID']}");
 header("X-INDEX: {$_SERVER['HTTP_X_DISPLAY_INDEX']}");
